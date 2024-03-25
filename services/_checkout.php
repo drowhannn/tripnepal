df98cef88d55e69690ad2fae019635454fc02579
Rohan Dhimal <drowhann@gmail.com>
2024-03-25 20:16:28 +0545

    feat: update eSewa payment integration

--- services/_checkout.php
+++ services/_checkout.php
@@ -9,96 +9,44 @@ $packageInstance = new Packages();
 $res = $packageInstance->getPackage($_GET['package']);
 $package = mysqli_fetch_assoc($res);
 
-// echo var_dump($user);
-// echo "<br>";
-// echo var_dump($package);
+$total_amount = $package['package_price'];
+$transaction_uuid = uniqid();
+$product_code = "EPAYTEST";
+
+$message = "total_amount=" . $total_amount . ",transaction_uuid=" . $transaction_uuid . ",product_code=" . $product_code;
+$secret = "8gBm/:&EnhH.1/q";
+
+$tax_amount = 0;
+$product_service_charge = 0;
+$product_delivery_charge = 0;
+$success_url = "http://localhost/triptrip/success.php";
+$failure_url = "http://localhost/triptrip/fail.php";
+$signed_field_names = "total_amount,transaction_uuid,product_code";
+$signature = base64_encode(hash_hmac('sha256', $message, $secret, true));
+?>
+
+<body>
+    <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST" style="display: none;" id="esewa_form">
+        <input type="text" id="amount" name="amount" value="<?php echo $total_amount; ?>" required>
+        <input type="text" id="tax_amount" name="tax_amount" value ="<?php echo $tax_amount; ?>" required>
+        <input type="text" id="total_amount" name="total_amount" value="<?php echo $total_amount; ?>" required>
+        <input type="text" id="transaction_uuid" name="transaction_uuid" value="<?php echo $transaction_uuid; ?>" required>
+        <input type="text" id="product_code" name="product_code" value ="<?php echo $product_code; ?>" required>
+        <input type="text" id="product_service_charge" name="product_service_charge" value="<?php echo $product_service_charge; ?>" required>
+        <input type="text" id="product_delivery_charge" name="product_delivery_charge" value="<?php echo $product_delivery_charge; ?>" required>
+        <input type="text" id="success_url" name="success_url" value="<?php echo $success_url; ?>" required>
+        <input type="text" id="failure_url" name="failure_url" value="<?php echo $failure_url; ?>" required>
+        <input type="text" id="signed_field_names" name="signed_field_names" value="<?php echo $signed_field_names; ?>" required>
+        <input type="text" id="signature" name="signature" value="<?php echo $signature; ?>" required>
+        <input value="Submit" type="submit">
+    </form>
+
+    <button id="submit" onclick="submitForm()">Pay with eSewa</button>
+    <script>
+        function submitForm() {
+            document.getElementById("esewa_form").submit();
+        }
+    </script>
+</body>
 
 
-/* PHP */
-$post_data = array();
-$post_data['store_id'] = "rooki64087f61151b1";
-$post_data['store_passwd'] = "rooki64087f61151b1@ssl";
-$post_data['total_amount'] = $package['package_price'];
-$post_data['currency'] = "BDT";
-$post_data['tran_id'] = "SSLCZ_TEST_" . uniqid();
-$post_data['success_url'] = "http://localhost/triptrip/success.php";
-$post_data['fail_url'] = "http://localhost/triptrip/fail.php";
-$post_data['cancel_url'] = "http://localhost/new_sslcz_gw/cancel.php";
-# $post_data['multi_card_name'] = "mastercard,visacard,amexcard";  # DISABLE TO DISPLAY ALL AVAILABLE
-
-# EMI INFO
-$post_data['emi_option'] = "1";
-$post_data['emi_max_inst_option'] = "9";
-$post_data['emi_selected_inst'] = "9";
-
-# CUSTOMER INFORMATION
-$post_data['cus_name'] = $user['username'];
-$post_data['cus_email'] = $user['email'];
-$post_data['cus_add1'] = "Dhaka";
-$post_data['cus_add2'] = "Dhaka";
-$post_data['cus_city'] = "Dhaka";
-$post_data['cus_state'] = "Dhaka";
-$post_data['cus_postcode'] = "1000";
-$post_data['cus_country'] = "Nepal";
-$post_data['cus_phone'] = "01711111111";
-$post_data['cus_fax'] = "01711111111";
-
-# SHIPMENT INFORMATION
-$post_data['ship_name'] = "Triptrip";
-$post_data['ship_add1 '] = "Dhaka";
-$post_data['ship_add2'] = "Dhaka";
-$post_data['ship_city'] = "Dhaka";
-$post_data['ship_state'] = "Dhaka";
-$post_data['ship_postcode'] = "1230";
-$post_data['ship_country'] = "Nepal";
-
-# OPTIONAL PARAMETERS
-$post_data['value_a'] = $user['id'];
-$post_data['value_b'] = $package['package_id'];
-$post_data['value_c'] = "ref003";
-$post_data['value_d'] = "ref004";
-
-
-$post_data['product_amount'] = $package['package_price'];
-$post_data['vat'] = "0";
-$post_data['discount_amount'] = "0";
-$post_data['convenience_fee'] = "0";
-
-# REQUEST SEND TO SSLCOMMERZ
-$direct_api_url = "https://sandbox.sslcommerz.com/gwprocess/v3/api.php";
-
-$handle = curl_init();
-curl_setopt($handle, CURLOPT_URL, $direct_api_url);
-curl_setopt($handle, CURLOPT_TIMEOUT, 30);
-curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 30);
-curl_setopt($handle, CURLOPT_POST, 1);
-curl_setopt($handle, CURLOPT_POSTFIELDS, $post_data);
-curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, FALSE); # KEEP IT FALSE IF YOU RUN FROM LOCAL PC
-
-
-$content = curl_exec($handle);
-
-$code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-
-if ($code == 200 && !(curl_errno($handle))) {
-    curl_close($handle);
-    $sslcommerzResponse = $content;
-} else {
-    curl_close($handle);
-    echo "FAILED TO CONNECT WITH SSLCOMMERZ API";
-    exit;
-}
-
-# PARSE THE JSON RESPONSE
-$sslcz = json_decode($sslcommerzResponse, true);
-
-if (isset($sslcz['GatewayPageURL']) && $sslcz['GatewayPageURL'] != "") {
-    # THERE ARE MANY WAYS TO REDIRECT - Javascript, Meta Tag or Php Header Redirect or Other
-    # echo "<script>window.location.href = '". $sslcz['GatewayPageURL'] ."';</script>";
-    echo "<meta http-equiv='refresh' content='0;url=" . $sslcz['GatewayPageURL'] . "'>";
-    # header("Location: ". $sslcz['GatewayPageURL']);
-    exit;
-} else {
-    echo "JSON Data parsing error!";
-}
